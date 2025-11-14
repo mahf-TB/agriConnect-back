3e220044be688f4aee5d13b6ef7644f4a05d8fac
mahf-TB <mahefatsilavirintsoa@gmail.com>
2025-11-14 18:52:36 +0300

    add  notificationService dans module

--- src/modules/commandes/cmd-produits/cmd-produits.module.ts
+++ src/modules/commandes/cmd-produits/cmd-produits.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { CmdProduitsService } from './cmd-produits.service';
 import { CmdProduitsController } from './cmd-produits.controller';
 import { PrismaService } from 'src/prisma.service';
+import { NotificationsModule } from 'src/modules/notifications/notifications.module';
 
 @Module({
   providers: [CmdProduitsService, PrismaService],
-  controllers: [CmdProduitsController]
+  controllers: [CmdProduitsController],
+  imports: [NotificationsModule],
 })
 export class CmdProduitsModule {}
